68a69a8de81c72957e9c48cb0d727ed757c558e7
Mohammad Dashti <mdashti@gmail.com>
2024-11-14 01:52:37 -0800

    Fixed a few issues and added more logging.

--- input/system/google_cloudsql/system.go
+++ input/system/google_cloudsql/system.go
@@ -3,6 +3,7 @@ package google_cloudsql
 import (
 	"context"
 	"fmt"
+	"log"
 	"time"
 
 	monitoring "cloud.google.com/go/monitoring/apiv3/v2"
@@ -16,22 +17,24 @@ import (
 
 var newMetricClientFunc = monitoring.NewMetricClient
 
-// GetSystemState - Gets system information about a Google Cloud SQL instance
 func GetSystemState(ctx context.Context, server *state.Server, logger *util.Logger) (system state.SystemState) {
-	logger.PrintInfo("CloudSQL/System: Called GetSystemState")
+	logger.PrintInfo("CloudSQL/System: Started GetSystemState")
 
 	config := server.Config
 	system.Info.Type = state.GoogleCloudSQLSystem
+	logger.PrintInfo("CloudSQL/System: Set system type to GoogleCloudSQLSystem")
 
 	if config.GcpCredentialsFile == "" && config.GcpProjectID == "" {
 		server.SelfTest.MarkCollectionAspectError(state.CollectionAspectSystemStats, "missing GCP credentials or project ID")
 		logger.PrintError("CloudSQL/System: Missing GCP credentials or project ID")
 		return
 	}
+	logger.PrintInfo("CloudSQL/System: Validated GCP credentials configuration")
 
 	var opts []option.ClientOption
 	if config.GcpCredentialsFile != "" {
 		opts = append(opts, option.WithCredentialsFile(config.GcpCredentialsFile))
+		logger.PrintInfo("CloudSQL/System: Using GCP credentials file: %s", config.GcpCredentialsFile)
 	}
 
 	client, err := newMetricClientFunc(ctx, opts...)
@@ -40,6 +43,7 @@ func GetSystemState(ctx context.Context, server *state.Server, logger *util.Logg
 		logger.PrintError("CloudSQL/System: Failed to create monitoring client: %v", err)
 		return
 	}
+	logger.PrintInfo("CloudSQL/System: Successfully created monitoring client")
 	defer client.Close()
 
 	instanceID := config.GcpCloudSQLInstanceID
@@ -48,8 +52,8 @@ func GetSystemState(ctx context.Context, server *state.Server, logger *util.Logg
 		config.GcpProjectID,
 		instanceID,
 	)
+	logger.PrintInfo("CloudSQL/System: Created filter for instance %s: %s", instanceID, filter)
 
-	// Create base request template
 	baseReq := &monitoringpb.ListTimeSeriesRequest{
 		Name:   fmt.Sprintf("projects/%s", config.GcpProjectID),
 		Filter: filter,
@@ -58,57 +62,85 @@ func GetSystemState(ctx context.Context, server *state.Server, logger *util.Logg
 			StartTime: &timestamppb.Timestamp{Seconds: time.Now().Add(-5 * time.Minute).Unix()},
 		},
 	}
+	logger.PrintInfo("CloudSQL/System: Created base request template")
 
 	// Memory stats
-	system.Memory.TotalBytes = uint64(getMemoryMetric(ctx, client, cloneRequest(baseReq), "cloudsql.googleapis.com/database/memory/quota"))
-	usedMemory := uint64(getMemoryMetric(ctx, client, cloneRequest(baseReq), "cloudsql.googleapis.com/database/memory/usage"))
-	system.Memory.FreeBytes = system.Memory.TotalBytes - usedMemory
-	totalUsageBytes := uint64(getMemoryMetric(ctx, client, cloneRequest(baseReq), "cloudsql.googleapis.com/database/memory/total_usage"))
-	bufferAndCacheBytes := totalUsageBytes - usedMemory
-	// TODO: currently, there is no way to separate the cache and buffer usage via CloudSQL metrics
-	// so we just use the total buffer and cache usage as the cached bytes
-	system.Memory.CachedBytes = bufferAndCacheBytes
-	system.Memory.SwapUsedBytes = uint64(getMemoryMetric(ctx, client, cloneRequest(baseReq), "cloudsql.googleapis.com/database/swap/bytes_used"))
+	logger.PrintInfo("CloudSQL/System: Starting memory metrics collection")
+	system.Memory.TotalBytes = uint64(getIntMetric(ctx, client, cloneRequest(baseReq), "cloudsql.googleapis.com/database/memory/quota"))
+	logger.PrintInfo("CloudSQL/System: Total memory bytes: %d", system.Memory.TotalBytes)
 
-	// CPU Stats with more detailed metrics
+	usedMemory := uint64(getIntMetric(ctx, client, cloneRequest(baseReq), "cloudsql.googleapis.com/database/memory/usage"))
+	logger.PrintInfo("CloudSQL/System: Used memory bytes: %d", usedMemory)
+
+	system.Memory.FreeBytes = system.Memory.TotalBytes - usedMemory
+	logger.PrintInfo("CloudSQL/System: Free memory bytes: %d", system.Memory.FreeBytes)
+
+	system.Memory.SwapUsedBytes = uint64(getIntMetric(ctx, client, cloneRequest(baseReq), "cloudsql.googleapis.com/database/swap/bytes_used"))
+	logger.PrintInfo("CloudSQL/System: Swap used bytes: %d", system.Memory.SwapUsedBytes)
+
+	// CPU Stats
+	logger.PrintInfo("CloudSQL/System: Starting CPU metrics collection")
 	system.CPUStats = make(state.CPUStatisticMap)
+	cpuUtil := getPercentMetric(ctx, client, cloneRequest(baseReq), "cloudsql.googleapis.com/database/cpu/utilization")
 	system.CPUStats["all"] = state.CPUStatistic{
 		DiffedOnInput: true,
 		DiffedValues: &state.DiffedSystemCPUStats{
-			UserPercent: getCPUMetric(ctx, client, cloneRequest(baseReq), "cloudsql.googleapis.com/database/cpu/utilization"),
+			UserPercent: cpuUtil,
 		},
 	}
+	logger.PrintInfo("CloudSQL/System: CPU utilization: %.2f%%", cpuUtil)
 
-	// Disk stats with I/O metrics
+	// Disk stats
+	logger.PrintInfo("CloudSQL/System: Starting disk metrics collection")
 	system.DiskStats = make(state.DiskStatsMap)
+	readOps := float64(getIntMetric(ctx, client, cloneRequest(baseReq), "cloudsql.googleapis.com/database/disk/read_ops_count"))
+	writeOps := float64(getIntMetric(ctx, client, cloneRequest(baseReq), "cloudsql.googleapis.com/database/disk/write_ops_count"))
+	diskUtil := float64(getPercentMetric(ctx, client, cloneRequest(baseReq), "cloudsql.googleapis.com/database/disk/utilization"))
+
 	system.DiskStats["default"] = state.DiskStats{
 		DiffedOnInput: true,
 		DiffedValues: &state.DiffedDiskStats{
-			ReadOperationsPerSecond:  float64(getDiskMetric(ctx, client, cloneRequest(baseReq), "cloudsql.googleapis.com/database/disk/read_ops_count")),
-			WriteOperationsPerSecond: float64(getDiskMetric(ctx, client, cloneRequest(baseReq), "cloudsql.googleapis.com/database/disk/write_ops_count")),
-			UtilizationPercent:       float64(getDiskMetric(ctx, client, cloneRequest(baseReq), "cloudsql.googleapis.com/database/disk/utilization")),
+			ReadOperationsPerSecond:  readOps,
+			WriteOperationsPerSecond: writeOps,
+			UtilizationPercent:       diskUtil,
 		},
 	}
+	logger.PrintInfo("CloudSQL/System: Disk metrics - Read ops: %.2f, Write ops: %.2f, Utilization: %.2f%%",
+		readOps, writeOps, diskUtil)
 
+	// Disk partitions
+	logger.PrintInfo("CloudSQL/System: Starting disk partition metrics collection")
 	system.DiskPartitions = make(state.DiskPartitionMap)
+	usedBytes := uint64(getIntMetric(ctx, client, cloneRequest(baseReq), "cloudsql.googleapis.com/database/disk/bytes_used"))
+	totalBytes := uint64(getIntMetric(ctx, client, cloneRequest(baseReq), "cloudsql.googleapis.com/database/disk/quota"))
+
 	system.DiskPartitions["/"] = state.DiskPartition{
 		DiskName:      "cloudsql",
 		PartitionName: "cloudsql",
-		UsedBytes:     uint64(getDiskMetric(ctx, client, cloneRequest(baseReq), "cloudsql.googleapis.com/database/disk/bytes_used")),
-		TotalBytes:    uint64(getDiskMetric(ctx, client, cloneRequest(baseReq), "cloudsql.googleapis.com/database/disk/quota")),
+		UsedBytes:     usedBytes,
+		TotalBytes:    totalBytes,
 	}
+	logger.PrintInfo("CloudSQL/System: Disk partition metrics - Used bytes: %d, Total bytes: %d",
+		usedBytes, totalBytes)
 
 	// Network stats
+	logger.PrintInfo("CloudSQL/System: Starting network metrics collection")
 	system.NetworkStats = make(state.NetworkStatsMap)
+	receivedBytes := uint64(getIntMetric(ctx, client, cloneRequest(baseReq), "cloudsql.googleapis.com/database/network/received_bytes_count"))
+	sentBytes := uint64(getIntMetric(ctx, client, cloneRequest(baseReq), "cloudsql.googleapis.com/database/network/sent_bytes_count"))
+
 	system.NetworkStats["default"] = state.NetworkStats{
 		DiffedOnInput: true,
 		DiffedValues: &state.DiffedNetworkStats{
-			ReceiveThroughputBytesPerSecond:  uint64(getNetworkMetric(ctx, client, cloneRequest(baseReq), "cloudsql.googleapis.com/database/network/received_bytes_count")),
-			TransmitThroughputBytesPerSecond: uint64(getNetworkMetric(ctx, client, cloneRequest(baseReq), "cloudsql.googleapis.com/database/network/sent_bytes_count")),
+			ReceiveThroughputBytesPerSecond:  receivedBytes,
+			TransmitThroughputBytesPerSecond: sentBytes,
 		},
 	}
+	logger.PrintInfo("CloudSQL/System: Network metrics - Received bytes/sec: %d, Sent bytes/sec: %d",
+		receivedBytes, sentBytes)
 
 	server.SelfTest.MarkCollectionAspectOk(state.CollectionAspectSystemStats)
+	logger.PrintInfo("CloudSQL/System: Successfully completed GetSystemState")
 
 	return
 }
@@ -121,83 +153,58 @@ func cloneRequest(req *monitoringpb.ListTimeSeriesRequest) *monitoringpb.ListTim
 	}
 }
 
-func getCPUMetric(ctx context.Context, client *monitoring.MetricClient, req *monitoringpb.ListTimeSeriesRequest, metricType string) float64 {
+func getPercentMetric(ctx context.Context, client *monitoring.MetricClient, req *monitoringpb.ListTimeSeriesRequest, metricType string) float64 {
+	log.Printf("Getting percentage metric for type %s", metricType)
 	req.Filter = fmt.Sprintf("%s AND metric.type = %q", req.Filter, metricType)
 
 	it := client.ListTimeSeries(ctx, req)
 	ts, err := it.Next()
 	if err == iterator.Done {
+		log.Printf("No percentage metrics found for type %s", metricType)
 		return 0
 	}
 	if err != nil {
+		log.Printf("Error fetching percentage metrics for type %s: %v", metricType, err)
 		return 0
 	}
 
 	if len(ts.Points) > 0 {
+		// Log all points for debugging
+		for i, point := range ts.Points {
+			log.Printf("Point %d: %v", i, point.Value.GetDoubleValue())
+		}
 		value := ts.Points[0].Value.GetDoubleValue() * 100
 		return value
 	}
 
+	log.Printf("No points found in percentage metrics for type %s", metricType)
 	return 0
 }
 
-func getMemoryMetric(ctx context.Context, client *monitoring.MetricClient, req *monitoringpb.ListTimeSeriesRequest, metricType string) int64 {
+func getIntMetric(ctx context.Context, client *monitoring.MetricClient, req *monitoringpb.ListTimeSeriesRequest, metricType string) int64 {
+	log.Printf("Getting integer metric for type %s", metricType)
 	req.Filter = fmt.Sprintf("%s AND metric.type = %q", req.Filter, metricType)
 
 	it := client.ListTimeSeries(ctx, req)
 	ts, err := it.Next()
 	if err == iterator.Done {
+		log.Printf("No integer metrics found for type %s", metricType)
 		return 0
 	}
 	if err != nil {
+		log.Printf("Error fetching integer metrics for type %s: %v", metricType, err)
 		return 0
 	}
 
 	if len(ts.Points) > 0 {
+		// Log all points for debugging
+		for i, point := range ts.Points {
+			log.Printf("Point %d: %v", i, point.Value.GetInt64Value())
+		}
 		value := ts.Points[0].Value.GetInt64Value()
 		return value
 	}
 
-	return 0
-}
-
-func getNetworkMetric(ctx context.Context, client *monitoring.MetricClient, req *monitoringpb.ListTimeSeriesRequest, metricType string) float64 {
-
-	req.Filter = fmt.Sprintf("%s AND metric.type = %q", req.Filter, metricType)
-
-	it := client.ListTimeSeries(ctx, req)
-	ts, err := it.Next()
-	if err == iterator.Done {
-		return 0
-	}
-	if err != nil {
-		return 0
-	}
-
-	if len(ts.Points) > 0 {
-		value := ts.Points[0].Value.GetDoubleValue()
-		return value
-	}
-
-	return 0
-}
-
-func getDiskMetric(ctx context.Context, client *monitoring.MetricClient, req *monitoringpb.ListTimeSeriesRequest, metricType string) int64 {
-	req.Filter = fmt.Sprintf("%s AND metric.type = %q", req.Filter, metricType)
-
-	it := client.ListTimeSeries(ctx, req)
-	ts, err := it.Next()
-	if err == iterator.Done {
-		return 0
-	}
-	if err != nil {
-		return 0
-	}
-
-	if len(ts.Points) > 0 {
-		value := ts.Points[0].Value.GetInt64Value()
-		return value
-	}
-
+	log.Printf("No points found in integer metrics for type %s", metricType)
 	return 0
 }
