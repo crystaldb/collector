cda0ef1fdae6ec4e0c51316c528dcdd569da90c7
Mohammad Dashti <mdashti@gmail.com>
2024-11-14 02:41:22 -0800

    Removed extra debugging.

--- input/system/google_cloudsql/system.go
+++ input/system/google_cloudsql/system.go
@@ -17,24 +17,20 @@ import (
 
 var newMetricClientFunc = monitoring.NewMetricClient
 
+// GetSystemState - Gets system information about a Google Cloud SQL instance
 func GetSystemState(ctx context.Context, server *state.Server, logger *util.Logger) (system state.SystemState) {
-	logger.PrintInfo("CloudSQL/System: Started GetSystemState")
-
 	config := server.Config
 	system.Info.Type = state.GoogleCloudSQLSystem
-	logger.PrintInfo("CloudSQL/System: Set system type to GoogleCloudSQLSystem")
 
 	if config.GcpCredentialsFile == "" && config.GcpProjectID == "" {
 		server.SelfTest.MarkCollectionAspectError(state.CollectionAspectSystemStats, "missing GCP credentials or project ID")
 		logger.PrintError("CloudSQL/System: Missing GCP credentials or project ID")
 		return
 	}
-	logger.PrintInfo("CloudSQL/System: Validated GCP credentials configuration")
 
 	var opts []option.ClientOption
 	if config.GcpCredentialsFile != "" {
 		opts = append(opts, option.WithCredentialsFile(config.GcpCredentialsFile))
-		logger.PrintInfo("CloudSQL/System: Using GCP credentials file: %s", config.GcpCredentialsFile)
 	}
 
 	client, err := newMetricClientFunc(ctx, opts...)
@@ -43,7 +39,6 @@ func GetSystemState(ctx context.Context, server *state.Server, logger *util.Logg
 		logger.PrintError("CloudSQL/System: Failed to create monitoring client: %v", err)
 		return
 	}
-	logger.PrintInfo("CloudSQL/System: Successfully created monitoring client")
 	defer client.Close()
 
 	instanceID := config.GcpCloudSQLInstanceID
@@ -52,8 +47,8 @@ func GetSystemState(ctx context.Context, server *state.Server, logger *util.Logg
 		config.GcpProjectID,
 		instanceID,
 	)
-	logger.PrintInfo("CloudSQL/System: Created filter for instance %s: %s", instanceID, filter)
 
+	// Create base request template
 	baseReq := &monitoringpb.ListTimeSeriesRequest{
 		Name:   fmt.Sprintf("projects/%s", config.GcpProjectID),
 		Filter: filter,
@@ -62,24 +57,17 @@ func GetSystemState(ctx context.Context, server *state.Server, logger *util.Logg
 			StartTime: &timestamppb.Timestamp{Seconds: time.Now().Add(-5 * time.Minute).Unix()},
 		},
 	}
-	logger.PrintInfo("CloudSQL/System: Created base request template")
 
 	// Memory stats
-	logger.PrintInfo("CloudSQL/System: Starting memory metrics collection")
 	system.Memory.TotalBytes = uint64(getIntMetric(ctx, client, cloneRequest(baseReq), "cloudsql.googleapis.com/database/memory/quota"))
-	logger.PrintInfo("CloudSQL/System: Total memory bytes: %d", system.Memory.TotalBytes)
 
 	usedMemory := uint64(getIntMetric(ctx, client, cloneRequest(baseReq), "cloudsql.googleapis.com/database/memory/usage"))
-	logger.PrintInfo("CloudSQL/System: Used memory bytes: %d", usedMemory)
 
 	system.Memory.FreeBytes = system.Memory.TotalBytes - usedMemory
-	logger.PrintInfo("CloudSQL/System: Free memory bytes: %d", system.Memory.FreeBytes)
 
 	system.Memory.SwapUsedBytes = uint64(getIntMetric(ctx, client, cloneRequest(baseReq), "cloudsql.googleapis.com/database/swap/bytes_used"))
-	logger.PrintInfo("CloudSQL/System: Swap used bytes: %d", system.Memory.SwapUsedBytes)
 
 	// CPU Stats
-	logger.PrintInfo("CloudSQL/System: Starting CPU metrics collection")
 	system.CPUStats = make(state.CPUStatisticMap)
 	cpuUtil := getPercentMetric(ctx, client, cloneRequest(baseReq), "cloudsql.googleapis.com/database/cpu/utilization")
 	system.CPUStats["all"] = state.CPUStatistic{
@@ -88,7 +76,6 @@ func GetSystemState(ctx context.Context, server *state.Server, logger *util.Logg
 			UserPercent: cpuUtil,
 		},
 	}
-	logger.PrintInfo("CloudSQL/System: CPU utilization: %.2f%%", cpuUtil)
 
 	system.Disks = make(state.DiskMap)
 	system.Disks["default"] = state.Disk{
@@ -96,7 +83,6 @@ func GetSystemState(ctx context.Context, server *state.Server, logger *util.Logg
 	}
 
 	// Disk stats
-	logger.PrintInfo("CloudSQL/System: Starting disk metrics collection")
 	system.DiskStats = make(state.DiskStatsMap)
 	readOps := float64(getIntMetric(ctx, client, cloneRequest(baseReq), "cloudsql.googleapis.com/database/disk/read_ops_count"))
 	writeOps := float64(getIntMetric(ctx, client, cloneRequest(baseReq), "cloudsql.googleapis.com/database/disk/write_ops_count"))
@@ -110,11 +96,8 @@ func GetSystemState(ctx context.Context, server *state.Server, logger *util.Logg
 			UtilizationPercent:       diskUtil,
 		},
 	}
-	logger.PrintInfo("CloudSQL/System: Disk metrics - Read ops: %.2f, Write ops: %.2f, Utilization: %.2f%%",
-		readOps, writeOps, diskUtil)
 
 	// Disk partitions
-	logger.PrintInfo("CloudSQL/System: Starting disk partition metrics collection")
 	system.DiskPartitions = make(state.DiskPartitionMap)
 	usedBytes := uint64(getIntMetric(ctx, client, cloneRequest(baseReq), "cloudsql.googleapis.com/database/disk/bytes_used"))
 	totalBytes := uint64(getIntMetric(ctx, client, cloneRequest(baseReq), "cloudsql.googleapis.com/database/disk/quota"))
@@ -125,11 +108,8 @@ func GetSystemState(ctx context.Context, server *state.Server, logger *util.Logg
 		UsedBytes:     usedBytes,
 		TotalBytes:    totalBytes,
 	}
-	logger.PrintInfo("CloudSQL/System: Disk partition metrics - Used bytes: %d, Total bytes: %d",
-		usedBytes, totalBytes)
 
 	// Network stats
-	logger.PrintInfo("CloudSQL/System: Starting network metrics collection")
 	system.NetworkStats = make(state.NetworkStatsMap)
 	receivedBytes := uint64(getIntMetric(ctx, client, cloneRequest(baseReq), "cloudsql.googleapis.com/database/network/received_bytes_count"))
 	sentBytes := uint64(getIntMetric(ctx, client, cloneRequest(baseReq), "cloudsql.googleapis.com/database/network/sent_bytes_count"))
@@ -141,11 +121,8 @@ func GetSystemState(ctx context.Context, server *state.Server, logger *util.Logg
 			TransmitThroughputBytesPerSecond: sentBytes,
 		},
 	}
-	logger.PrintInfo("CloudSQL/System: Network metrics - Received bytes/sec: %d, Sent bytes/sec: %d",
-		receivedBytes, sentBytes)
 
 	server.SelfTest.MarkCollectionAspectOk(state.CollectionAspectSystemStats)
-	logger.PrintInfo("CloudSQL/System: Successfully completed GetSystemState")
 
 	return
 }
@@ -159,7 +136,6 @@ func cloneRequest(req *monitoringpb.ListTimeSeriesRequest) *monitoringpb.ListTim
 }
 
 func getPercentMetric(ctx context.Context, client *monitoring.MetricClient, req *monitoringpb.ListTimeSeriesRequest, metricType string) float64 {
-	log.Printf("Getting percentage metric for type %s", metricType)
 	req.Filter = fmt.Sprintf("%s AND metric.type = %q", req.Filter, metricType)
 
 	it := client.ListTimeSeries(ctx, req)
@@ -174,10 +150,6 @@ func getPercentMetric(ctx context.Context, client *monitoring.MetricClient, req 
 	}
 
 	if len(ts.Points) > 0 {
-		// Log all points for debugging
-		for i, point := range ts.Points {
-			log.Printf("Point %d: %v", i, point.Value.GetDoubleValue())
-		}
 		value := ts.Points[0].Value.GetDoubleValue() * 100
 		return value
 	}
@@ -187,7 +159,6 @@ func getPercentMetric(ctx context.Context, client *monitoring.MetricClient, req 
 }
 
 func getIntMetric(ctx context.Context, client *monitoring.MetricClient, req *monitoringpb.ListTimeSeriesRequest, metricType string) int64 {
-	log.Printf("Getting integer metric for type %s", metricType)
 	req.Filter = fmt.Sprintf("%s AND metric.type = %q", req.Filter, metricType)
 
 	it := client.ListTimeSeries(ctx, req)
@@ -202,10 +173,6 @@ func getIntMetric(ctx context.Context, client *monitoring.MetricClient, req *mon
 	}
 
 	if len(ts.Points) > 0 {
-		// Log all points for debugging
-		for i, point := range ts.Points {
-			log.Printf("Point %d: %v", i, point.Value.GetInt64Value())
-		}
 		value := ts.Points[0].Value.GetInt64Value()
 		return value
 	}
